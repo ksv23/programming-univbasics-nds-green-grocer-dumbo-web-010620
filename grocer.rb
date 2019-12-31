79e66bddfc3fb36442db39810d61439b27ad0dff
Kring Villanueva <ksvillanueva7@gmail.com>
2019-12-31 21:52:23 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -102,7 +102,7 @@ def checkout(cart, coupons)
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
   consolidate_cart = consolidate_cart(cart)
-  couponed_cart = apply_coupons(consolidate_cart)
+  couponed_cart = apply_coupons(consolidate_cart, coupons)
   total_cart = apply_clearance(couponed_cart)
 
   total = 0
