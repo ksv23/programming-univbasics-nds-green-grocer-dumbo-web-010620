c92363f99d03f2285779bba65945d5cf0290d3b7
Kring Villanueva <ksvillanueva7@gmail.com>
2019-12-31 20:43:51 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,19 +1,50 @@
 require 'pry'
 
-def find_item_by_name_in_collection(name, collection)
+def find_item_by_name_in_collection(name, collection) #array of hashes
   # Implement me first!
   #
   # Consult README for inputs and outputs
 
-binding.pry
+    #1. iterate array of hashes
+    #2. check each item if 
+  row_index = 0
 
+  while row_index < collection.length do
+    if collection[row_index][:item] == name
+      return collection[counter]          #return will exit the method and will return collection[counter] return the item IF true
+    end
+    row_index += 1
+  end
+
+  nil
 end
 
+    #1. return array of hashes
 def consolidate_cart(cart)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+  new_cart = []
+  row_index = 0
+
+  while row_index < cart.length do
+      new_item = find_item_by_name_in_collection(cart[counter][:item], new_cart)
+
+      if new_item != nil 
+          new_item[row_index] += 1
+      else
+        new_item = {
+          :item => cart[counter][:item],
+          :price => cart[counter][:price],
+          :clearance => cart[counter][:clearance],
+          :count => 1
+        } 
+        new_cart << new_item
+      end
+      row_index += 1
+  end
+  new_cart
 end
 
 def apply_coupons(cart, coupons)
